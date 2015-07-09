5697dfd0270e72d296c2ada8668cf05e5381e43c
Ashlyn Hallman <ashlynbrookeh@gmail.com>
2015-07-09 00:28:21 -0400

    fixed

--- gnt/site_editor.rb
+++ gnt/site_editor.rb
@@ -1,6 +1,6 @@
 require_relative 'site_user'
 class SiteEditor < SiteUser
-  def promote_user(ueer_to_promote)
+  def promote_user(user_to_promote)
     #this function allows editors to promote users to writers or editors
   end
 end
