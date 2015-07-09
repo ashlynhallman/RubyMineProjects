6fa8fc3aaa4ef8084a4c399aa82d675f97ec28f4
Ronald Hallman <dhallman@ulive.com>
2015-07-09 00:15:49 -0400

    Added new article function to site editor.

--- gnt/site_writer.rb
+++ gnt/site_writer.rb
@@ -1,3 +1,10 @@
 require_relative 'site_user'
+
 class SiteWriter < SiteUser
+
+  def write_article
+    # This function exists only on Site Editors
+    # because they have authority to author new
+    # content.
+  end
 end
